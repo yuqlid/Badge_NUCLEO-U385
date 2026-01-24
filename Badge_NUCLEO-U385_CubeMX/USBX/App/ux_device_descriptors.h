1660753825df497fdc783df54de6afa2693b0e52
yuqlid <yuqlid@gmail.com>
2026-01-24 14:02:00 +0900

    Update CubeMX

--- Badge_NUCLEO-U385_CubeMX/USBX/App/ux_device_descriptors.h
+++ Badge_NUCLEO-U385_CubeMX/USBX/App/ux_device_descriptors.h
@@ -219,7 +219,7 @@ uint16_t USBD_Get_Configuration_Number(uint8_t class_type, uint8_t interface_typ
 
 #define USBD_VID                                      1155
 #define USBD_PID                                      22288
-#define USBD_LANGID_STRING                            1041
+#define USBD_LANGID_STRING                            1033
 #define USBD_MANUFACTURER_STRING                      "STMicroelectronics"
 #define USBD_PRODUCT_STRING                           "STM32 USB Device"
 #define USBD_SERIAL_NUMBER                            "000000000001"
